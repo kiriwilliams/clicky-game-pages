ad1d9d003a3279365b81870b287639a6766994af
sharky <zeebing@gmail.com>
2019-06-24 18:02:57 -0400

    updated project name

--- precache-manifest.17aeb668745f2c66266311ba2df96f5c.js
+++ precache-manifest.17aeb668745f2c66266311ba2df96f5c.js
@@ -1,22 +1,22 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "2d067e1f94acb410febbe15b588baa01",
-    "url": "/clicky-game-pages/index.html"
+    "url": "/pokemon-memory-game/index.html"
   },
   {
     "revision": "d15eef082e455a799039",
-    "url": "/clicky-game-pages/static/css/main.2cce8147.chunk.css"
+    "url": "/pokemon-memory-game/static/css/main.2cce8147.chunk.css"
   },
   {
     "revision": "58f496bc1ce27350f0eb",
-    "url": "/clicky-game-pages/static/js/2.98dcc9dd.chunk.js"
+    "url": "/pokemon-memory-game/static/js/2.98dcc9dd.chunk.js"
   },
   {
     "revision": "d15eef082e455a799039",
-    "url": "/clicky-game-pages/static/js/main.d57e6c27.chunk.js"
+    "url": "/pokemon-memory-game/static/js/main.d57e6c27.chunk.js"
   },
   {
     "revision": "b7e45184c9bba87eaaa2",
-    "url": "/clicky-game-pages/static/js/runtime~main.619b02fb.js"
+    "url": "/pokemon-memory-game/static/js/runtime~main.619b02fb.js"
   }
 ]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/clicky-game-pages/precache-manifest.17aeb668745f2c66266311ba2df96f5c.js"
+  "/pokemon-memory-game/precache-manifest.17aeb668745f2c66266311ba2df96f5c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/clicky-game-pages/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pokemon-memory-game/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
