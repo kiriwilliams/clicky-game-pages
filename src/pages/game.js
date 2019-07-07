a1f70e7d094ade006c33c0304d4b545a42be7696
sharky <zeebing@gmail.com>
2019-07-07 13:15:34 -0400

    made into functional component

--- src/pages/game.js
+++ src/pages/game.js
@@ -1,103 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
 import Img from "../components/img";
 import Nav from "../components/nav";
-import pokemon from "../pokemon.json";
-
-
-class Game extends Component {
-    state = {
-        currentScore: 0,
-        highScore: 0,
-        pokemon: pokemon
-    }
-
-    endGame = () => {
-        alert("game over");
-        if(this.state.currentScore > this.state.highScore){
-            this.setState({highScore: this.state.currentScore });
-        }
-        this.setState({ currentScore: 0 });
-
-        let updatePokemon = this.state.pokemon;
-        updatePokemon.forEach(pokemon => pokemon.clicked = false);
-        this.setState({ pokemon: updatePokemon});
-        this.shuffle();
-    }
-
-    shuffle = () => {
-        //Fisher-Yates (aka Knuth) Shuffle.
-        function shuffle(array) {
-            var currentIndex = array.length, temporaryValue, randomIndex;
-
-            // While there remain elements to shuffle...
-            while (0 !== currentIndex) {
-
-                // Pick a remaining element...
-                randomIndex = Math.floor(Math.random() * currentIndex);
-                currentIndex -= 1;
-
-                // And swap it with the current element.
-                temporaryValue = array[currentIndex];
-                array[currentIndex] = array[randomIndex];
-                array[randomIndex] = temporaryValue;
-            }
-
-            return array;
-        }
-        this.setState({ pokemon: shuffle(this.state.pokemon) });
-    }
-
-    incrementScore = () => {
-        this.setState({ currentScore: this.state.currentScore + 1 })
-    }
-
-    handleClick = event => {
-        event.preventDefault();
-        let pokeArray = this.state.pokemon;
-
-        const id = event.target.getAttribute("id");
-        let clickedPokemon = pokeArray.find(poke => {
-            return poke.id === parseInt(id)
-        });
-        
-
-        //end the game if image has already been clicked
-        if(clickedPokemon.clicked){
-            this.endGame();
-        }
-        //set clicked to true and shuffle
-        else{
-            clickedPokemon.clicked = true;
-            // event.target.setState({ clicked: true });
-            this.setState({pokemon: pokeArray})
-            this.shuffle();
-            this.incrementScore();
-        }
-
-    }
 
 
 
-    render() {
-        return (
-            <div>
-                <div className="row">
-                    <div className="col-12">
-                    <Nav currentScore={this.state.currentScore} highScore={this.state.highScore}/>
-                    </div>
-                </div>
-                <div className="row container mx-auto mt-5">
-                    <div className="col-12 text-center bg-white p-3">
-                            {this.state.pokemon.map(pokemon => {
-                                return (
-                                    <Img key={pokemon.id} id={pokemon.id} src={pokemon.src} handleClick={this.handleClick} endGame={this.endGame} shuffle={this.shuffle} incrementScore={this.incrementScore} reset={this.reset} />
-                                )
-                            })}
-                    </div>
+function Game(props) {
+
+    return (
+        <div>
+            <div className="row">
+                <div className="col-12">
+                    <Nav currentScore={props.currentScore} highScore={props.highScore} toggleModal={props.toggleModal} />
                 </div>
             </div>
-        )
-    }
+            <div className="row container mx-auto mt-5">
+                <div className="col-12 text-center bg-white p-3">
+                    {props.pokemon.map(pokemon => {
+                        return (
+                            <Img key={pokemon.id} id={pokemon.id} src={pokemon.src} handleClick={props.handleClick} endGame={props.endGame} shuffle={props.shuffle} incrementScore={props.incrementScore} reset={props.reset} />
+                        )
+                    })}
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Game;
